1db02ae9f77b4883157804162b4cf3bfc0860220
Imran A <74223481+CubeWolv@users.noreply.github.com>
2024-03-03 19:24:38 +0300

    Update urls.py

--- home/urls.py
+++ home/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-
+from django.contrib.auth.views import LogoutView
 from . import views
 
 urlpatterns = [
@@ -9,4 +9,6 @@ urlpatterns = [
     path('aboutus', views.aboutus, name='about'),
     path('termsofuse', views.termsofuse, name='termsofuse'),
     path('contact', views.contact, name='contact'),
+    path('logout/', LogoutView.as_view(next_page='home'), name='logout'),
+
 ]
